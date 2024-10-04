ace9b59a8553398c3e278d1b1804df3409c4152f
Lucas Silva <8760513+lucasrgsilva@users.noreply.github.com>
2024-10-04 16:01:03 -0300

    add group to aggrid

--- src/app/ag-grid/ag-grid.component.ts
+++ src/app/ag-grid/ag-grid.component.ts
@@ -1,6 +1,6 @@
 import { AgGridAngular } from '@ag-grid-community/angular';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { BarSparklineOptions, ColDef, GridApi, GridReadyEvent, ModuleRegistry, ValueFormatterParams } from '@ag-grid-community/core';
+import { BarSparklineOptions, ColDef, GridApi, GridReadyEvent, INumberFilterParams, ModuleRegistry, RowGroupingDisplayType, RowSelectionOptions, ValueFormatterParams } from '@ag-grid-community/core';
 import '@ag-grid-community/styles/ag-grid.css';
 import '@ag-grid-community/styles/ag-theme-quartz.css';
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
@@ -39,44 +39,63 @@ export class AgGridComponent implements OnInit {
   themeClass = 'ag-theme-quartz';
   private gridApi!: GridApi<People>;
   peopleService = inject(PeopleService);
+  rowSelection: RowSelectionOptions | "single" | "multiple" = {
+    mode: "multiRow",
+    groupSelects: "self",
+  };
+  rowGroupPanelShow: "always" | "onlyWhenGrouping" | "never" = "always";
+  groupDisplayType: RowGroupingDisplayType = "multipleColumns";
   pagination = true;
   paginationPageSize = 50;
+  autoGroupColumnDef: ColDef = {
+    minWidth: 200,
+    filter: "agGroupColumnFilter",
+  };
   paginationPageSizeSelector = [10, 20, 50, 100];
-  defaultColDef: ColDef = { 
+  defaultColDef: ColDef = {
     flex: 1,
     filter: true,
     floatingFilter: true,
     editable: true,
+    enableRowGroup: true,
   };
   rowData: People[] = [];
   colDefs: ColDef<People>[] = [
-    { field: 'full_name', headerName: 'Contact Name' },
-    { field: 'job_title', headerName: 'Job Title' },
-    { field: 'country', headerName: 'Country',
+    {
+      field: 'full_name', headerName: 'Contact Name', filter: "agTextColumnFilter", filterParams: {
+        buttons: ["apply"],
+      } as INumberFilterParams
+    },
+    { field: 'job_title', headerName: 'Job Title', enableRowGroup: true, filter: "agTextColumnFilter" },
+    {
+      field: 'country', headerName: 'Country',
       cellRenderer: CountryLogoRenderer,
       cellClass: "logoCell",
-      minWidth: 50, },
+      minWidth: 50,
+    },
     { field: 'is_online', headerName: 'Status' },
-    { field: 'registered', headerName: 'Registered', cellDataType: 'date' },
-    { field: 'target', headerName: 'Engagement', cellRenderer: "agSparklineCellRenderer", cellRendererParams: {
-      sparklineOptions: {
-        type: "bar",
-        label: {enabled: true},
-        valueAxisDomain: [0, 100],
-      } as BarSparklineOptions,
-    }},
-    { field: 'budget', headerName: 'Budget', valueFormatter: currencyFormatter },
-    { field: 'phone', headerName: 'Phone' },
-    { field: 'address', headerName: 'Address' },
+    { field: 'registered', headerName: 'Registered', cellDataType: 'date', filter: "agDateColumnFilter", },
+    {
+      field: 'target', headerName: 'Engagement', cellRenderer: "agSparklineCellRenderer", cellRendererParams: {
+        sparklineOptions: {
+          type: "bar",
+          label: { enabled: true },
+          valueAxisDomain: [0, 100],
+        } as BarSparklineOptions,
+      }
+    },
+    { field: 'budget', filter: "agNumberColumnFilter", headerName: 'Budget', valueFormatter: currencyFormatter },
+    { field: 'phone', headerName: 'Phone', filter: "agTextColumnFilter" },
+    { field: 'address', headerName: 'Address', filter: "agTextColumnFilter" },
   ];
 
   ngOnInit(): void {
-    this.peopleService.getPeople().pipe(map(array => array.map((item) => ({ 
-      ...item, 
+    this.peopleService.getPeople().pipe(map(array => array.map((item) => ({
+      ...item,
       target: Array.isArray(item.target) ? item.target : [item.target],
-      registered: new Date(typeof item.registered === 'string' ? item.registered.replace(/ /g,'') : item.registered)
+      registered: new Date(typeof item.registered === 'string' ? item.registered.replace(/ /g, '') : item.registered)
     }))))
-    .subscribe((data) => this.rowData = data);
+      .subscribe((data) => this.rowData = data);
   }
 
   onBtExport() {
